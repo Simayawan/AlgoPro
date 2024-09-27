1e9ff6821de4991e593f322e0ac799732ad622c2
Simayawan <allenxavinzky.adjiewibowo@binus.ac.id>
2024-09-27 18:22:43 +0700

    Tuples.py

--- Week 4/Tuples.py
+++ Week 4/Tuples.py
@@ -1,7 +1,7 @@
 def strtup(tuple, string): # defines the function
 
     new_word = '' #makes an empty variable to store data for the final output.
-    for x in range(len(string)): # the range function turns the string into a range of numbers starting from 0 until the amount of letters there are in the word.
+    for x in range(len(string)): # the range function turns the string into a range of numbers starting from 0 until the amount of letters (but minus 1 from the actual length of the string) there are in the word.
         new_word += string[x] * tuple[x] # this line basically adds a new letter in the word every time it was multiplied by the numbers assigned to it in the tuple.
 
     if len(tuple) != len(string): # this line basically means that if the length of the tuple is not the same as the length of the string, it will print out the statement below.
